783a8dcb9ee3569326fc9459f50eec2ed79b646b
Pham Tuong Nguyen <npham50@my.centennialcollege.ca>
2024-02-12 17:09:16 -0500

    ASSIGNMENT3

--- ASSIGNMENT3/PhamTuongNguyen_COMP254Lab3_Ex2/Main.java
+++ ASSIGNMENT3/PhamTuongNguyen_COMP254Lab3_Ex2/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the string: ");
         String m = sc.next();
-        if(isPalindrome(m)){
+        if(isPalindrome2(m, 0, m.length() - 1)){
             System.out.printf("%s is a palindrome\n", m);
         }
         else{
@@ -18,5 +18,10 @@ public class Main {
     public static boolean isPalindrome(String str){
         if(str.length() == 1 || str.length() == 0) return true;
         else return str.charAt(0) == str.charAt(str.length() - 1) && isPalindrome(str.substring(1, str.length() - 1));
+    }
+    //Alternative
+    public static boolean isPalindrome2(String str, int s, int e){
+        if(s >= e) return true;
+        else return str.charAt(s) == str.charAt(e) && isPalindrome2(str, s + 1, e - 1);
     }
 }
