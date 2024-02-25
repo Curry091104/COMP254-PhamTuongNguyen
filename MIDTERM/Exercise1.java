c34740b03d98550dd1afddd19ae87fe75f1de955
Pham Tuong Nguyen <npham50@my.centennialcollege.ca>
2024-02-25 08:17:34 -0500

    Remove

--- MIDTERM/Exercise1.java
+++
@@ -1,7 +0,0 @@
-public class Exercise1{
-    public static int maxElement(int[] A, int n){
-        if(n==0) return A[n];
-        return Math.max(A[n], maxElement(A, n-1));
-    }
-    //Running time of this function is linear time, it based on the size of the array 
-}
